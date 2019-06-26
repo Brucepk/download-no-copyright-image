27149933014701c6c00346812f2d827ef1516df4
brucepk <>
2019-06-26 23:37:34 +0800

    commit code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# download-no-copyright-image
-Python批量下载无版权图片
+# Python批量下载无版权图片
+
+源码见 py文件
+
+更多文章可在公众号「Python知识圈」查看
+
+![公众号](https://github.com/Brucepk/pk.github.io/blob/master/gzh.jpg)
---
+++ pixabay.py
@@ -0,0 +1,63 @@
+import requests
+import re
+import random
+import time
+import os
+
+
+def get_urls(url):
+    headers = {
+             'User-Agent':
+             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+             '(KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+    html = requests.get(url, headers=headers).text
+    urls = re.compile(r'data-lazy-srcset="(.*?)"')
+    res = re.findall(urls, html)
+    img_urls = []
+    for u in res:
+        img_url = str(u).split(',')[0].strip(' 1x')
+        img_urls.append(img_url)
+    return img_urls
+
+
+# 创建不同的文件夹储存图片
+def mkdir(directory):
+    isExists = os.path.exists(r'/Users/brucepk/Pictures/素材图片/{}'.format(directory))
+    if not isExists:
+        print('创建 %s 目录' % directory)
+        os.makedirs(r'/Users/brucepk/Pictures/素材图片/{}'.format(directory))   # 创建目录
+        os.chdir(r'/Users/brucepk/Pictures/素材图片/{}'.format(directory))      # 切换到创建的文件夹
+        return True
+    else:
+        print('目录 %s 已存在，即将保存！' % directory)
+        return False
+
+
+def download(filename, url):
+    with open(filename, 'wb+') as f:
+        try:
+            f.write(requests.get(url).content)
+            print('成功下载图片：', filename)
+        except:
+            print('下载失败：', filename)
+
+
+if __name__ == '__main__':
+    directory = input('输出你要下载的图片的英文关键字：')
+    for num in range(1, 6):
+        search_url = 'https://pixabay.com/images/search/{}/?pagi={}'.format(directory, num)  # 拼接每页的链接
+        mkdir(directory)
+        for url in get_urls(search_url):
+            filename = r'/Users/brucepk/Pictures/素材图片/{}/'.format(directory) + url.split('/')[-1]  # 图片的路径
+            download(filename, url)
+        print('第%s页已爬取' % num)
+        time.sleep(int(format(random.randint(3, 10))))     # 随机等待时间
+
+
+
+
+
+
+
+
+
